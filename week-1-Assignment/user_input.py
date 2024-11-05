fd2a9350dfdf3a2158e186dce9f9f41f28788480
Yungprnz <yemightie@gmail.com>
2024-11-05 16:18:48 +0100

    Week 1 Assignment completed.

---
+++ week-1-Assignment/user_input.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ')
+age = int(input('How old are you? '))
+location = input('Where is your current location? ')
+
+print(f'Hello {name}! You are {age} years old and live in {location}.')
